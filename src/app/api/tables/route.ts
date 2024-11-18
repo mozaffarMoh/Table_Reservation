09ea3ea315609c11757dca64d9e5e989d0ceb369
mozaffarMoh <ferawwwess@gmail.com>
2024-11-18 19:51:47 +0300

    new-era6

--- src/app/api/tables/route.ts
+++ src/app/api/tables/route.ts
@@ -6,10 +6,19 @@ import { isTimesReserved } from '@/constant/isTimesReserved';
 
 
 export async function GET(req: NextRequest) {
-  
-  try{
-    return NextResponse.json({ success: true, data: 'success after edit' });
-  }catch (error) {
+
+ try {
+    const client = await clientPromise;
+    const db = client.db('tables');
+
+
+    let tables = await db.collection('types').find({}).toArray();
+
+ 
+
+    return NextResponse.json({ success: true, data: tables });
+  }
+   catch (error) {
     return NextResponse.json({ success: false, message: 'خطأ في الاتصال بقاعدة البيانات' }, { status: 500 });
   }
 }
