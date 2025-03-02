dd9d36dd4b50362c4b163709fb203e3d4e823bf8
mozaffarMoh <ferawwwrss@gmail.com>
2025-03-03 02:08:20 +0300

    remove-push-notification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Main from '@/sections/Main/Main';
-import PushNotification from '@/sections/SendNotification/SendNotification';
 import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <h1>Push Notification</h1>
-      <PushNotification />
+      <Main />
     </>
   );
 };
