d4626cb042402e7e4a935a745f1c0000f5bca87c
mozaffarMoh <ferawwwrss@gmail.com>
2025-03-03 02:46:09 +0300

    add-push-notification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Main from '@/sections/Main/Main';
+import PushNotification from '@/sections/SendNotification/SendNotification';
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <Main />
+      {/*    <Main /> */}
+      <Typography>PUSH NOTIFIAITON</Typography>
+      <PushNotification />
     </>
   );
 };
